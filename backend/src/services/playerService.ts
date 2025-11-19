e30c83439ed3b507c50e621ee5764dd013dde934
Emmi Quirin <emmi.quirin@forefront.se>
2025-11-19 15:11:24 +0100

    getAllPlayers from database function

---
+++ backend/src/services/playerService.ts
@@ -0,0 +1,12 @@
+import { Player } from "@shared/validation/types";
+import { pool } from "../db/config";
+import { RowDataPacket } from "mysql2/promise";
+
+interface PlayerRow extends RowDataPacket, Player {}
+
+export async function getAllPlayers(): Promise<Player[]> {
+  const [rows] = await pool.query<PlayerRow[]>(
+    "SELECT id, name FROM players ORDER BY id"
+  );
+  return rows as Player[];
+}
