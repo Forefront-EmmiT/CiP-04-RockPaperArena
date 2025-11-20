02d77f0be8b2b46c940a654cef8f55867dd7b4c5
Emmi Quirin <emmi.quirin@forefront.se>
2025-11-20 15:39:35 +0100

    Changed file name

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
-import cors from 'cors';
-import dotenv from 'dotenv';
+import cors from "cors";
+import dotenv from "dotenv";
 import playerRoutes from "./routes/playerRoutes";
 
 dotenv.config();
