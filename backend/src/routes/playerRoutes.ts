6dc0c2e71cdbe3f69b5943de1e0fa1840a15dfeb
Emmi Quirin <emmi.quirin@forefront.se>
2025-11-21 15:44:18 +0100

    Routs for schedule and result

--- backend/src/routes/playerRoutes.ts
+++ backend/src/routes/playerRoutes.ts
@@ -2,10 +2,16 @@ import { Router } from "express";
 
 import {
   getHello,
+  getPlayers,
+  getTournamentSchedule,
+  getTournamentResult,
 } from "../controllers/playerController";
 
 const router = Router();
 
 router.get("/", getHello);
+router.get("/players", getPlayers);
+router.get("/schedule", getTournamentSchedule);
+router.get("/result", getTournamentResult);
 
 export default router;
